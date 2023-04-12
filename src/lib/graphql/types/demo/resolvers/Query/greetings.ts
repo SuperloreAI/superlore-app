eca12d597d9bb469957a9a1e9384265368288b76
0xterran <96885027+0xterran@users.noreply.github.com>
2023-04-13 01:08:15 +0700

    fixed graphql resolver types

--- src/lib/graphql/types/demo/resolvers/Query/greetings.ts
+++ src/lib/graphql/types/demo/resolvers/Query/greetings.ts
@@ -1,3 +1,8 @@
-
-        import type   { QueryResolvers } from './../../../types.generated';
-        export const greetings: NonNullable<QueryResolvers['greetings']> = async (_parent, _arg, _ctx) => { /* Implement Query.greetings resolver logic here */ };
+import type { QueryResolvers } from "./../../../types.generated";
+export const greetings: NonNullable<QueryResolvers["greetings"]> = async (
+  _parent,
+  _arg,
+  _ctx
+) => {
+  /* Implement Query.greetings resolver logic here */ return "greetings";
+};
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import "@/lib/secrets/secrets.ts";
+import "@/lib/secrets/secrets";
 
-import { companies, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -14,11 +14,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const company = await prisma.companies.create({
+  const user = await prisma.users.create({
     data: {
-      name: "Test Company",
+      email: "test@User.us",
+      name: "Test User",
     },
   });
-  console.log(company);
-  res.status(200).json(company);
+  console.log(user);
+  res.status(200).json(user);
 }

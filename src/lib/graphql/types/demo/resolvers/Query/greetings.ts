d2fd7e5b63f26d738fea17e77a27336d57e1f5ff
Superlore <129238308+Superlore@users.noreply.github.com>
2023-04-13 01:34:05 +0700

    Revert "fixed graphql resolver types"

--- src/lib/graphql/types/demo/resolvers/Query/greetings.ts
+++ src/lib/graphql/types/demo/resolvers/Query/greetings.ts
@@ -1,8 +1,3 @@
-import type { QueryResolvers } from "./../../../types.generated";
-export const greetings: NonNullable<QueryResolvers["greetings"]> = async (
-  _parent,
-  _arg,
-  _ctx
-) => {
-  /* Implement Query.greetings resolver logic here */ return "greetings";
-};
+
+        import type   { QueryResolvers } from './../../../types.generated';
+        export const greetings: NonNullable<QueryResolvers['greetings']> = async (_parent, _arg, _ctx) => { /* Implement Query.greetings resolver logic here */ };

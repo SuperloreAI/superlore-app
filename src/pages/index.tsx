7566c62ba9034d8dbeb94a00faede11f9d29fc85
Kangze Huang <kangzeroo@gmail.com>
2023-04-17 12:32:56 +0800

    updated homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,7 +59,7 @@ export default function Home() {
         </Link>
 
         <Link
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          href="/mascot/create-mascot"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
